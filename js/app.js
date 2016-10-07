d3dc5506387f1d14839ffcd35969cbd7c4510fb3
Aaron <apurjh@gmail.com>
2016-10-07 15:20:04 +0100

    Added random prey generator

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
-$(function(){
+// $(function(){
   console.log('loaded');
   var gridWidth = 20;
   var gridHeight = 20;
   for(var x=gridHeight;x>=1;x--){
     for(var y=1;y<=gridWidth;y++){
-      $('.content').append("<div class='cell' id='cell" + y + "-"+ x + "'>(" + y + "_" + x + ")</div>");
-      console.log("<div class='cell' id='cell" + y + "-"+ x + ")'>(" + y + "," + x + ")</div>");
+      $('.content').append("<div class='cell' id='cell" + y + "-"+ x + "'>(" + y + "," + x + ")</div>");
     }
   }
 
@@ -13,4 +12,10 @@ $(function(){
   $('#cell3-1').addClass('snakeHead');
   $('#cell2-1').addClass('snakeBody');
   $('#cell1-1').addClass('snakeTail');
-});
+
+  function addPrey(){
+    var xPrey = Math.floor(Math.random()*gridWidth);
+    var yPrey = Math.floor(Math.random()*gridWidth);
+    $('#cell' + xPrey + '-' + yPrey).addClass('prey');
+  }
+// });
