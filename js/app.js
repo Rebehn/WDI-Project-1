d3b48d0128ee6990e202495720bf7013eae3d797
Aaron <apurjh@gmail.com>
2016-10-07 14:54:39 +0100

    Changed cell selectors so x and y axis are in correct direction and started snake

--- js/app.js
+++ js/app.js
@@ -2,10 +2,15 @@ $(function(){
   console.log('loaded');
   var gridWidth = 20;
   var gridHeight = 20;
-  for(var x=1;x<=gridWidth;x++){
-    for(var y=1;y<=gridHeight;y++){
-      $('.content').append("<div class='cell' id='cell(" + x + ","+ y + ")'>(" + x + "," + y + ")</div>");
-      console.log("<div class='cell' id='cell(" + x + ","+ y + ")'>(" + x + "," + y + ")</div>");
+  for(var x=gridHeight;x>=1;x--){
+    for(var y=1;y<=gridWidth;y++){
+      $('.content').append("<div class='cell' id='cell" + y + "-"+ x + "'>(" + y + "_" + x + ")</div>");
+      console.log("<div class='cell' id='cell" + y + "-"+ x + ")'>(" + y + "," + x + ")</div>");
     }
   }
+
+  console.log($('#cell3-1'));
+  $('#cell3-1').addClass('snakeHead');
+  $('#cell2-1').addClass('snakeBody');
+  $('#cell1-1').addClass('snakeTail');
 });
